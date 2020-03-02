6c4a609b058211e520c1a7d940a1303d592ec3de
massimo <coruxrome@gmail.com>
2020-03-02 19:43:15 +0100

    Create classi utente, listino e profilo

---
+++ Java/src/model/Listino.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.List;
+
+public class Listino {
+	Long idListino;
+	String dataInizio;
+	String dataFine;
+	List<Prodotto> listino;
+}
--- Java/src/model/Utente.java
+++ Java/src/model/Utente.java
@@ -1,5 +1,12 @@
 package model;
 
 public class Utente {
-
+	Long idUtente;
+	String nome;
+	String cognome;
+	String username;
+	String password;
+	String mail;
+	
+	
 }
