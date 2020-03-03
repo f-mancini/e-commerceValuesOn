fd75096a60d5d5ff325f93dad8ec58f74002c438
serenigianmarco <serenigianmarco@yahoo.it>
2020-03-03 11:45:13 +0100

    aggiunta ServletLogin.java

---
+++ Java/src/controller/ServletLogin.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+
+
+@WebServlet(name = "ServletLogin", urlPatterns = {"*.do"})
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 2L;
+       
+    
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        StringBuffer str = new StringBuffer(request.getServletPath());
+        String comando = str.substring(1,str.lastIndexOf(".do"));
+        String pagina ="";
+        RequestDispatcher rd;
+        ServletContext sc;
+        
+        switch(comando){
+            case "login":
+               
+                break;
+            
+            case "annulla":
+               
+                break;
+                                          
+                              
+        }
+        sc = getServletContext();
+        rd = sc.getRequestDispatcher(pagina);
+        rd.forward(request, response);
+	}
+
+	
+}
