ccd827e71132a9c4a5b54ad8dd81e8f22697688a
massimo <coruxrome@gmail.com>
2020-03-04 09:53:09 +0100

    Auto stash before merge of "master" and "origin/master"

--- Java/src/controller/Servletdb.java
+++ Java/src/controller/Servletdb.java
@@ -1,9 +1,13 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import repository.DatabaseConnection;
 
@@ -16,5 +20,23 @@ public class Servletdb extends HttpServlet {
     
 	protected void CreateTable() throws SQLException {
 		   DatabaseConnection.creazioneTabelle();
+  
+	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		 try {
+			DatabaseConnection.creazioneTabelle();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+	
+	
 }
